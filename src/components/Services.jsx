446758f009479df5172e81ec5d82e38d5081e8e5
Prabhat Kumar <83307497+2012prabhat@users.noreply.github.com>
2024-12-15 23:55:17 +0530

    Services section completed

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import './css/Services.css'
+import globe from './images/globe.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleRight  } from '@fortawesome/free-solid-svg-icons';
+import ui from './images/ui.svg'
+import ecommerce from './images/ecommerce.svg';
+import api from './images/api.svg'
+import seo from './images/seo.svg'
+import fullStack from './images/fullStack.svg'
+
+
 
 export default function Services() {
+  const cardData = [
+    { title: 'Web Development', desc: 'Building fast, responsive, and modern websites tailored to your needs.',icon:globe },
+    { title: 'UI/UX Design', desc: 'Creating visually appealing and user-friendly designs.',icon:ui },
+    { title: 'Full-Stack Development', desc: 'Providing complete web solutions with front-end and back-end expertise.',icon:fullStack },
+    { title: 'E-commerce Solutions', desc: 'Building secure and scalable online stores to help grow your business.',icon:ecommerce },
+    { title: 'API Integration', desc: 'Seamlessly connecting your services and data for optimal performance.',icon:api},
+    { title: 'SEO Optimization', desc: 'Helping your website rank higher on search engines with effective SEO strategies.',icon:seo }
+  ];
   return (
-    <div>Services</div>
+    <div className="servicesCont">
+      <h1>Let’s Build Something Exceptional</h1>
+      <h3>"Whether it’s a sleek portfolio, an engaging e-commerce platform, or a robust web application, I’m here to make it happen."</h3>
+      <div className="cardsCont">
+      {cardData.map((card, index) => (
+        <div className="card" key={index}>
+          <img src={card.icon} alt="" />
+          <div className="title">{card.title}</div>
+          <div className="desc">{card.desc}</div>
+          <button>Learn More  <FontAwesomeIcon style={{ marginLeft: '10px', fontSize: '15px' }} icon={faArrowCircleRight} /></button>
+        </div>
+      ))}
+      </div>
+    </div>
   )
 }
