17ff0c22545c03d65fbb027a98e877026734c1e5
Prabhat Kumar <83307497+2012prabhat@users.noreply.github.com>
2024-12-27 14:03:23 +0530

    Add files via upload

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import './css/Services.css'
-import globe from './images/globe.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleRight  } from '@fortawesome/free-solid-svg-icons';
-import ui from './images/ui.svg'
-import ecommerce from './images/ecommerce.svg';
-import api from './images/api.svg'
-import seo from './images/seo.svg'
-import fullStack from './images/fullStack.svg'
+import cardData from './ServicesData'
 
 
 
 export default function Services() {
-  const cardData = [
-    { title: 'Web Development', desc: 'Building fast, responsive, and modern websites tailored to your needs.',icon:globe },
-    { title: 'UI/UX Design', desc: 'Creating visually appealing and user-friendly designs.',icon:ui },
-    { title: 'Full-Stack Development', desc: 'Providing complete web solutions with front-end and back-end expertise.',icon:fullStack },
-    { title: 'E-commerce Solutions', desc: 'Building secure and scalable online stores to help grow your business.',icon:ecommerce },
-    { title: 'API Integration', desc: 'Seamlessly connecting your services and data for optimal performance.',icon:api},
-    { title: 'SEO Optimization', desc: 'Helping your website rank higher on search engines with effective SEO strategies.',icon:seo }
-  ];
   return (
     <div className="servicesCont">
       <h1>Let’s Build Something Exceptional</h1>
---
+++ src/components/ServicesData.jsx
@@ -0,0 +1,41 @@
+import ui from "./images/ui.svg";
+import ecommerce from "./images/ecommerce.svg";
+import api from "./images/api.svg";
+import seo from "./images/seo.svg";
+import globe from "./images/globe.svg";
+import fullStack from "./images/fullStack.svg";
+
+const cardData = [
+  {
+    title: "Web Development",
+    desc: "Building fast, responsive, and modern websites tailored to your needs.",
+    icon: globe,
+  },
+  {
+    title: "UI/UX Design",
+    desc: "Creating visually appealing and user-friendly designs.",
+    icon: ui,
+  },
+  {
+    title: "Full-Stack Development",
+    desc: "Providing complete web solutions with front-end and back-end expertise.",
+    icon: fullStack,
+  },
+  {
+    title: "E-commerce Solutions",
+    desc: "Building secure and scalable online stores to help grow your business.",
+    icon: ecommerce,
+  },
+  {
+    title: "API Integration",
+    desc: "Seamlessly connecting your services and data for optimal performance.",
+    icon: api,
+  },
+  {
+    title: "SEO Optimization",
+    desc: "Helping your website rank higher on search engines with effective SEO strategies.",
+    icon: seo,
+  },
+];
+
+export default cardData
